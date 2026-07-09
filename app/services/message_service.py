acf86032b5b43b6584c80833c2c5fad5b8ca162c
root <root@static.211.172.164.185.ip.webhost1.net>
2026-07-09 13:49:59 +0200

    Track and cleanup service messages

--- app/services/message_service.py
+++ app/services/message_service.py
@@ -5,6 +5,7 @@ from aiogram.types import Message
 
 class MessageService:
     LAST_SERVICE_MESSAGE_KEY = "last_service_message_id"
+    SERVICE_MESSAGE_IDS_KEY = "service_message_ids"
 
     @staticmethod
     async def delete_message(message: Message | None) -> None:
@@ -30,6 +31,44 @@ class MessageService:
             pass
 
     @staticmethod
+    async def delete_service_messages(message: Message, state: FSMContext) -> None:
+        data = await state.get_data()
+
+        message_ids = list(data.get(MessageService.SERVICE_MESSAGE_IDS_KEY) or [])
+
+        old_last_id = data.get(MessageService.LAST_SERVICE_MESSAGE_KEY)
+        if old_last_id and old_last_id not in message_ids:
+            message_ids.append(old_last_id)
+
+        for message_id in message_ids:
+            await MessageService.delete_message_by_id(message, message_id)
+
+        await state.update_data(
+            **{
+                MessageService.SERVICE_MESSAGE_IDS_KEY: [],
+                MessageService.LAST_SERVICE_MESSAGE_KEY: None,
+            }
+        )
+
+    @staticmethod
+    async def remember_service_message(
+        state: FSMContext,
+        message_id: int,
+    ) -> None:
+        data = await state.get_data()
+        message_ids = list(data.get(MessageService.SERVICE_MESSAGE_IDS_KEY) or [])
+
+        if message_id not in message_ids:
+            message_ids.append(message_id)
+
+        await state.update_data(
+            **{
+                MessageService.SERVICE_MESSAGE_IDS_KEY: message_ids,
+                MessageService.LAST_SERVICE_MESSAGE_KEY: message_id,
+            }
+        )
+
+    @staticmethod
     async def replace_service_message(
         message: Message,
         state: FSMContext,
@@ -38,18 +77,37 @@ class MessageService:
         delete_user_message: bool = True,
         **kwargs,
     ) -> Message:
-        data = await state.get_data()
-        last_service_message_id = data.get(MessageService.LAST_SERVICE_MESSAGE_KEY)
-
-        await MessageService.delete_message_by_id(message, last_service_message_id)
+        await MessageService.delete_service_messages(message, state)
 
         if delete_user_message:
             await MessageService.delete_message(message)
 
         sent_message = await message.answer(text, **kwargs)
 
-        await state.update_data(
-            **{MessageService.LAST_SERVICE_MESSAGE_KEY: sent_message.message_id}
+        await MessageService.remember_service_message(
+            state,
+            sent_message.message_id,
+        )
+
+        return sent_message
+
+    @staticmethod
+    async def send_service_message(
+        message: Message,
+        state: FSMContext,
+        text: str,
+        *,
+        delete_user_message: bool = False,
+        **kwargs,
+    ) -> Message:
+        if delete_user_message:
+            await MessageService.delete_message(message)
+
+        sent_message = await message.answer(text, **kwargs)
+
+        await MessageService.remember_service_message(
+            state,
+            sent_message.message_id,
         )
 
         return sent_message
